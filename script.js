4c546bb77e1fa86648ae3bd9f1ba7da4790ea226
lakpoo <kiliandominik93@gmail.com>
2023-03-30 09:55:37 +0200

    CSS

---
+++ script.js
@@ -0,0 +1,15 @@
+const premier = document.getElementById("premier");
+const second = document.getElementById("second");
+const troisieme = document.getElementById("troisieme");
+
+premier.addEventListener("click", function() {
+  premier.style.display = "none";
+});
+
+second.addEventListener("click", function() {
+  second.style.display = "none";
+});
+
+troisieme.addEventListener("click", function() {
+  troisieme.style.display = "none";
+});
